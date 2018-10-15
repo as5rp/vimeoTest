ab198a3267af2b6248f03e485859c690e7c5c705
Alexandra Stampfl <alexandrastampfl@studentsMBP102.local>
2018-10-14 21:41:33 -0400

    first page

--- vimeo/src/App.js
+++ vimeo/src/App.js
@@ -5,8 +5,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <p>
-            Hello World</p>
+      <div className="monsoon">
+        <img src={'./monsoon.jpg'} alt="Monsoon" style={{float: 'left', padding: '20px', verticalAlign: 'middle'}}/>
+        <h2> MONSOON III </h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+
+        <div className="beams">
+            <img src={'./beams.jpg'} alt="Beams" style={{float: 'right', padding: '20px', verticalAlign: 'middle'}}/>
+            <h2> BEAMS</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+
+        <div className="move">
+          <img src={'./movie2.jpg'} alt="Move 2" style={{float: 'left', padding: '20px', verticalAlign: 'middle'}}/>
+          <h2> MOVE 2</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
       </div>
     );
   }
